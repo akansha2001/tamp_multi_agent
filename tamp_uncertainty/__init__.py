a8a598145dceabd81977f97bef86f5ea7feeb2f2
akansha2001 <mukherjeeakansha07@gmail.com>
2025-06-13 12:05:20 +0200

    tamp_uncertainty for class uncertainty and search for object

---
+++ tamp_uncertainty/__init__.py
@@ -0,0 +1,35 @@
+# Copyright (c) 2022-2024, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+import gymnasium as gym
+import os
+
+from . import class_uncertainty_ik_abs_env_cfg, search_object_ik_abs_env_cfg
+
+
+
+##
+# Inverse Kinematics - Absolute Pose Control
+##
+
+
+
+gym.register(
+    id="Isaac-Lift-Franka-IK-Abs-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    kwargs={
+        "env_cfg_entry_point": class_uncertainty_ik_abs_env_cfg.FrankaLiftEnvCfg,
+    },
+    disable_env_checker=True,
+)
+
+gym.register(
+    id="Isaac-Lift-Franka-IK-Abs-v1",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    kwargs={
+        "env_cfg_entry_point": search_object_ik_abs_env_cfg.FrankaLiftEnvCfg,
+    },
+    disable_env_checker=True,
+)
+
